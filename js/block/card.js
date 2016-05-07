e786c816790b86f1113c0423a0f1f77e055cff00
atoyansk <atoyansk@gmail.com>
2016-05-08 00:52:00 +0300

    Add cards
    Changes to add cards.

--- js/block/card.js
+++ js/block/card.js
@@ -1,35 +1,32 @@
-var $cards = document.querySelectorAll('.card');
-var $cardColors = document.querySelectorAll('.card-options');
+var $wrapCard = document.querySelector('.wrap-card');
+var $cardOptions = document.querySelectorAll('.card-options');
+$wrapCard.addEventListener('click', function (e) {
+  var $this = e.target;
+  var $card = $this.parentNode.parentNode.parentNode;
+  var $cardContent = $card.querySelector('.card-content');
 
-for (var i = 0; i < $cards.length; i++) {
-  $cards[i].addEventListener('click', function (e) {
-    var $this = e.target;
-    var $card = this;
-    var $cardContent = $card.querySelector('.card-content');
+  if ($this.dataset.color) {
+    $card.dataset.color = $this.dataset.color;
 
-    if ($this.dataset.color) {
-      $card.dataset.color = $this.dataset.color;
-
-      for (var p = 0; p < $cardColors.length; p++) {
-        $cardColors[p].classList.remove('isActive');
-      };
-
-      $this.classList.add('isActive');
+    for (var p = 0; p < $cardOptions.length; p++) {
+      $cardOptions[p].classList.remove('isActive');
     };
 
-    if ($this.classList.contains('card_delete')){
-      $card.remove();
+    $this.classList.add('isActive');
+  };
+
+  if ($this.classList.contains('card_delete')){
+    $card.remove();
+  }
+  if ($this.classList.contains('card_edit')){
+    if($cardContent.getAttribute('contenteditable') == 'false'){
+      $cardContent.setAttribute('contenteditable', 'true');
+      $cardContent.focus();
+      $this.classList.add('isActive');
+    }else{
+      $cardContent.setAttribute('contenteditable', 'false');
+      $cardContent.blur();
+      $this.classList.remove('isActive');
     }
-    if ($this.classList.contains('card_edit')){
-      if($cardContent.getAttribute('contenteditable') == 'false'){
-        $cardContent.setAttribute('contenteditable', 'true');
-        $cardContent.focus();
-        $this.classList.add('isActive');
-      }else{
-        $cardContent.setAttribute('contenteditable', 'false');
-        $cardContent.blur();
-        $this.classList.remove('isActive');
-      }
-    }
-  });
-};
+  }
+});
